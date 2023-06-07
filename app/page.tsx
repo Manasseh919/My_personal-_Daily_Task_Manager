26b6e994cee490d5eccd60329e6e61261175b515
Mann U <mannu@Manassehs-Laptop.local>
2023-06-07 09:22:09 +0000

    working on Board 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
+import Board from "@/components/Board";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main className="">
       {/* Header */}
-      <Header/>
-
+      <Header />
+      
       {/* Board */}
-      <h1>My Daily task 1.1</h1>
+      <Board />
     </main>
   );
 }
